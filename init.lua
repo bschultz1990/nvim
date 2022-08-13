7983fd1e8fe4c7a6a05a61dab06bb9b1f596d0ce
Ben S <bschultz1990@users.noreply.github.com>
2022-08-12 21:58:11 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@
 print ("vim.g.neovide: ",vim.g.neovide)
 
 -- GENERAL
-vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard:append = {"unnamedplus"}
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.mouse = "a"
@@ -26,6 +26,7 @@ vim.opt.expandtab = false
 vim.opt.ignorecase = true
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
+vim.opt.winblend = 20
 
 
 -- NEOVIDE
