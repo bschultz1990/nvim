36bd7739de46f5ac11b9271d35d1e6f313361d10
Ben Schultz <ben@schultz.com>
2022-08-17 13:57:08 -0500

    Added files via CMD

--- lua/cfg_nvim-lspconfig.lua
+++ lua/cfg_nvim-lspconfig.lua
@@ -1,11 +1,17 @@
 -- connect to servers
 -- read more at :h vim.lsp.buf<TAB>
-require('lspconfig').tsserver.setup{
-    capabilities = capabilities
-    on_attach = function()
+
+-- Need more servers?
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+local nvim_lsp = require('lspconfig')
+local servers = {'tsserver', 'sumneko_lua'}
+
+for i, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup{
+    on_attach = function(var)
     vim.keymap.set("n", "<C-k>", vim.lsp.buf.hover, {buffer=0})
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
     vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
     vim.keymap.set("n", "gr", vim.lsp.buf.references, {buffer=0})
   end,
-}
+  }
