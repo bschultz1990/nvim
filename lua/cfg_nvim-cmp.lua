db7915abef7a8ac884d08a63053a21612fd7f221
Ben Schultz <bschultz1990@gmail.com>
2022-08-17 21:00:36 -0500

    more dusting with lspconfig...

--- lua/cfg_nvim-cmp.lua
+++ lua/cfg_nvim-cmp.lua
@@ -1,16 +1,15 @@
   -- Setup lspconfig.
   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-require('lspconfig').tsserver.setup {
-    on_attach = function()
-      print ('attached! :)')
-    vim.keymap.set("n", "<C-k>", vim.lsp.buf.hover, {buffer=0})
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
-    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, {buffer=0})
-  end,
-    capabilities = capabilities,
-}
-  -- Setup nvim-cmp.
+-- require('lspconfig').tsserver.setup {
+--     on_attach = function()
+--     vim.keymap.set("n", "<C-k>", vim.lsp.buf.hover, {buffer=0})
+--     vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
+--     vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
+--     vim.keymap.set("n", "gr", vim.lsp.buf.references, {buffer=0})
+--   end,
+--     capabilities = capabilities,
+-- }
+--   -- Setup nvim-cmp.
   vim.opt.completeopt = {"menu", "menuone", "noselect"}
   local cmp = require'cmp'
 
--- lua/cfg_nvim-lspconfig.lua
+++ lua/cfg_nvim-lspconfig.lua
@@ -1,10 +1,16 @@
+-- Do we have an lspconfig in the building?
+local status_ok, _ = pcall(require, 'lspconfig')
+if not status_ok then return end
+----------------------------------------
 -- connect to servers
 -- read more at :h vim.lsp.buf<TAB>
 -- Need more servers?
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+----------------------------------------
+
 Servers = {'tsserver', 'sumneko_lua'}
 
-for i, lsp in ipairs(servers) do
+for i, lsp in ipairs(Servers) do
   require('lspconfig')[lsp].setup{
   on_attach = function()
     print(lsp..' ATTACHED! :D')
@@ -13,33 +19,27 @@ for i, lsp in ipairs(servers) do
     nmap('gD','vim.lsp.buf.declaration')
     nmap('<leader>r','vim.lsp.buf.rename')
     nmap('gr','vim.lsp.buf.references')
+
+    if (lsp == 'sumneko_lua') then
+      local settings = {
+	Lua = {
+	  runtime = {
+	    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+	    version = 'LuaJIT',
+	  },
+	  diagnostics = {
+	    -- Get the language server to recognize the `vim` global
+	    globals = {'vim',},
+	  },
+	  workspace = {
+	    -- Make the server aware of Neovim runtime files
+	    library = vim.api.nvim_get_runtime_file("", true),
+	  },
+	  telemetry = {enable = false,},
+	},
+      }
+    end
+
   end
   }
 end
-
--- for i, lsp in ipairs(servers) do
---   nvim_lsp[lsp].setup{
---     on_attach = function()
-    -- print(i..' attached!')
-  -- end,
--- }
-    -- if (lsp == 'sumneko_lua') then
-    --   settings = {
-	-- Lua = {
-	  -- runtime = {
-	    -- -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-	    -- version = 'LuaJIT',
-	  -- },
-	  -- diagnostics = {
-	    -- -- Get the language server to recognize the `vim` global
-	    -- globals = {'vim'},
-	  -- },
-	  -- workspace = {
-	    -- -- Make the server aware of Neovim runtime files
-	    -- library = vim.api.nvim_get_runtime_file("", true),
-	  -- },
-	  -- telemetry = {
-	    -- enable = false,
-	  -- },
-	-- },
-    --   },
