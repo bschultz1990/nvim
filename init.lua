9bfa44961d646248bdb31f6ee1c288d98d9f9d7e
Ben S <bschultz1990@users.noreply.github.com>
2022-08-19 22:08:21 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -77,6 +77,7 @@ Plug 'nvim-telescope/telescope-file-browser.nvim'
 ---- NVIM-LSP
 Plug 'neovim/nvim-lspconfig'
 Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-nvim-lua'
 Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'hrsh7th/cmp-path'
 Plug 'L3MON4D3/LuaSnip'
