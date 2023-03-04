76ec0336780be7052b3cb5d673e936efeb663699
Ben S <bens@noemail.com>
2023-03-03 22:29:04 -0600

    Pruned some color schemes. Moved TabNine config to its own file. Removed plugins backup file. Deleted old session files. Renamed some internal vim functions. Added a list of colorschemes in init.lua for easy application.

--- lua/configs/tabnine_c.lua
+++ lua/configs/tabnine_c.lua
@@ -1,12 +1,18 @@
 local M = {}
 
-require('tabnine').setup({
-		disable_auto_comment=true,
-		-- accept_keymap="<Tab>",
-		dismiss_keymap = "<C-]>",
-		debounce_ms = 800,
-		suggestion_color = {gui = "#808080", cterm = 244},
-		execlude_filetypes = {"TelescopePrompt"}
-	})
+local tabnine = require('cmp_tabnine.config')
+
+tabnine:setup({
+	max_lines = 1000,
+	max_num_results = 1,
+	sort = true,
+	run_on_every_keystroke = false,
+	snippet_placeholder = 'TN',
+	ignored_file_types = {
+		md = true,
+		txt = true
+	},
+	show_prediction_strength = false
+})
 
 return M
