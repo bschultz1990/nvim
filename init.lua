17e28fb03b11cdd2800a123ba0f9aa636ea0d061
Ben Schultz <Ben_Schultz@vfc.com>
2023-06-13 16:26:48 -0500

    Switch the plugin manager to Plug.nvim

--- init.lua
+++ init.lua
@@ -1,7 +1,16 @@
 require('functions')
 require('base')
 require('keymaps')
-require('plugins')
+vim.cmd([[
+call plug#begin()
+  Plug 'windwp/nvim-autopairs'
+  Plug 'stevearc/oil.nvim'
+call plug#end()
+]])
+
+require("oil").setup()
+vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
+
 --require('keymaps_plugins_c')
 --require('test')
 
--- lua/plugins.vim
+++ lua/plugins.vim
@@ -1,3 +1,3 @@
 call plug#begin()
-
+  Plug 'windwp/nvim-autopairs'
 call plug#end()
