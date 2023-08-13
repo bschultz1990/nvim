221c16e03a2f468a7c832c63390962f842bd4a94
Ben S <bens@noemail.com>
2023-08-12 20:44:51 -0500

    Clean up init.lua

--- init.lua
+++ init.lua
@@ -2,20 +2,16 @@ require('functions')
 require('base')
 require('keymaps')
 require('plugins')
-require ('keymaps_plugins')
-vim.cmd('colorscheme '..
-  -- 'neosolarized'
-  -- 'moonlight'
-  -- 'night-owl'
-  -- 'sonokai'
-  'catppuccin'
-  -- 'palenight'
-  -- 'tokyodark'
-  -- 'tokyonight'
-  -- 'default'
-  )
+require('keymaps_plugins')
+vim.cmd('colorscheme ' ..
+	-- 'neosolarized'
+	-- 'moonlight'
+	-- 'night-owl'
+	-- 'sonokai'
+	'catppuccin'
+-- 'palenight'
+-- 'tokyodark'
+-- 'tokyonight'
+-- 'default'
+)
 print(' init.lua loaded! :)')
-
-
--- Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-
