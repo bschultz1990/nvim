7bf6c3f252fb9fa4fde45bcf1ab5bf6d270b7bac
Ben Schultz <Ben_Schultz@vfc.com>
2024-05-09 10:34:40 -0500

    Automatically grab VimPlug.

--- init.lua
+++ init.lua
@@ -1,7 +1,15 @@
 require('functions')
 require('base')
 require('keymaps')
+
+
 vim.cmd([[
+let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
+if empty(glob(data_dir . '/autoload/plug.vim'))
+    silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+      autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+endif
+
 call plug#begin()
 
 Plug 'windwp/nvim-autopairs'
