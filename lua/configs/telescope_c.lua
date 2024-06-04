5c45405d75d9b6c5bd5fff4d3261620c3c23485a
Ben S <bschultz1990@users.noreply.github.com>
2024-06-04 14:43:53 -0500

    Update telescope_c.lua fallback options
    Live grep and find files from project git root with fallback.

--- lua/configs/telescope_c.lua
+++ lua/configs/telescope_c.lua
@@ -29,9 +29,51 @@ require('telescope').setup {
 		}
 	}
 }
+
+-- Live grep from project git root with fallback
+function live_grep_from_project_git_root()
+	local function is_git_repo()
+		vim.fn.system("git rev-parse --is-inside-work-tree")
+
+		return vim.v.shell_error == 0
+	end
+
+	local function get_git_root()
+		local dot_git_path = vim.fn.finddir(".git", ".;")
+		return vim.fn.fnamemodify(dot_git_path, ":h")
+	end
+
+	local opts = {}
+
+	if is_git_repo() then
+		opts = {
+			cwd = get_git_root(),
+		}
+	end
+
+	require("telescope.builtin").live_grep(opts)
+end
+
+-- Find files from project git root with fallback
+function vim.find_files_from_project_git_root()
+  local function is_git_repo()
+    vim.fn.system("git rev-parse --is-inside-work-tree")
+    return vim.v.shell_error == 0
+  end
+  local function get_git_root()
+    local dot_git_path = vim.fn.finddir(".git", ".;")
+    return vim.fn.fnamemodify(dot_git_path, ":h")
+  end
+  local opts = {}
+  if is_git_repo() then
+    opts = {
+      cwd = get_git_root(),
+    }
+  end
+  require("telescope.builtin").find_files(opts)
+end
+
 -- Load Extra Extensions
 require("telescope").load_extension "file_browser"
 require("telescope").load_extension "ui-select"
-
-
 return M
