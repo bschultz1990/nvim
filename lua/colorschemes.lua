7fed15898122a525e8d3b6acedcfd5eddfbfd6a3
Ben <bschultz1990@gmail.com>
2025-08-18 18:30:32 -0400

    More COc

--- lua/colorschemes.lua
+++ lua/colorschemes.lua
@@ -1,9 +1,9 @@
 -- DARK
-vim.cmd("colorscheme miniautumn")
+-- vim.cmd("colorscheme miniautumn")
 -- vim.cmd("colorscheme minicyan")
 -- vim.cmd("colorscheme slate")
 -- vim.cmd("colorscheme minischeme")
 
 -- LIGHT
 -- vim.cmd("colorscheme peachpuff")
--- vim.cmd("colorscheme morning")
+vim.cmd("colorscheme morning")
