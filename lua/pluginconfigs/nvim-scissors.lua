5fee8021cdef2d0fff544ea7e88952bfba1866c5
Ben <bschultz1990@gmail.com>
2025-08-20 20:48:19 -0500

    More snippets

--- lua/pluginconfigs/nvim-scissors.lua
+++ lua/pluginconfigs/nvim-scissors.lua
@@ -8,16 +8,4 @@ return {
     { '<leader>sa', ':ScissorsAddNewSnippet<cr>', { desc = "Snippet: Edit" }, mode = 'n' },
     { '<leader>se', ':ScissorsEditSnippet<cr>', { desc = "Snippet: Add" }, mode = { 'x', 'n' } }
   },
-  config = function ()
-    -- vim.api.nvim_create_autocmd("BufWritePre", {
-    --   desc = "Format snippets on save using ",
-    --   pattern = ".json",
-    --   group = vim.api.nvim_create_augroup("groupname", { clear = true }),
-    --   callback = function()
-    --     vim.cmd'!cd '.. UserSnippetDir
-    --     vim.cmd'!find  -name "*.json" | xargs -I {} yq --inplace --output-format=json "sort_keys(..)" {}'
-    --   end,
-    -- })
-  end
-
 }
