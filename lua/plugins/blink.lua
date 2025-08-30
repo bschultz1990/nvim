881a48ca414d5ba287e5018270a8765a52f0c4f3
Ben <bschultz1990@gmail.com>
2025-08-30 09:48:18 -0500

    More snippet scaffolding

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,7 +2,7 @@ return {
   'saghen/blink.cmp',
   dependencies = { 'L3MON4D3/LuaSnip' },
   version = '1.*',
-  event = 'BufReadPost',
+  event = {'BufReadPost', 'CmdlineEnter'},
 
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
