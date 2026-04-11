5f28672e60ebd350166a5e43ab6cc8f16b9cfa81
= <=>
2026-04-11 11:06:10 -0500

    nvim-treesitter set local C compiler to 'gcc' if on Windows

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,18 +1,35 @@
+-- MIGRATION GUIDES
+-- https://www.qu8n.com/posts/treesitter-migration-guide-for-nvim-0-12
+-- https://github.com/nvim-treesitter/nvim-treesitter/tree/main
+
+
+-- PREREQUISITES:
+-- scoop install tree-sitter
+-- c compiler
+
+
 return {
   "nvim-treesitter/nvim-treesitter",
   branch = 'main', --main branch for Neovim 0.12 and beyond
   lazy = false,
   build = ":TSUpdate",
+
+
   -- This plugin does not support lazy loading.
   -- You MUST run config = function() instead of opts = {}
 
-    config = function()
-      require('nvim-treesitter').setup {
-        auto_install = true,
-        sync_install = true,
-        -- highlight = { enable = true, },
-        -- indent = { enable = true, },
-      }
+
+  config = function()
+    require('nvim-treesitter').setup {
+      auto_install = true,
+      sync_install = true,
+      install_dir = vim.fn.stdpath('data') .. '/site' -- parser install directory
+    }
+
+    -- Set local c compiler to 'gcc' if on Windows
+    if vim.loop.os_uname().sysname == "Windows_NT" then
+      vim.env.CC = "gcc"
+    end
 
     --   local ensure_installed = {
     --     "lua",
@@ -33,11 +50,15 @@ return {
     --
     -- Enable highlighting and indentation
     vim.api.nvim_create_autocmd('FileType', {
+      desc = "Enable TreeSitter highlighting and indentation",
+      pattern = "*", -- filetype
       callback = function()
-        pcall(vim.treesitter.start) -- highlighting
-        vim.bo.indentexpr = "v:lua.require'mvin-treesitter'.indentexpr()" -- indentation
+        -- Enable treesitter highlighting and disable regex syntax
+        pcall(vim.treesitter.start)
+        -- Enable treesitter-based indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
       end,
     })
-  -- }
-end
+
+  end,
 }
