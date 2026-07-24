cf6ebfd3b9d50ed3cdab7835001f03b8837b7740
Ben Schultz <bschultz1990@gmail.com>
2026-07-23 19:05:19 -0500

    Add go snippets.

--- init.lua
+++ init.lua
@@ -1,3 +1,3 @@
 -- TODO: Source this when you figure out pkg_generator
--- vim.cmd('runtime! lua/config/pkg_generator/init.lua')
+vim.cmd('runtime! lua/config/pkg_generator/init.lua')
 vim.cmd('runtime! lua/*.lua')
--- lua/ui2.lua
+++ lua/ui2.lua
@@ -1,23 +1,17 @@
 require('vim._core.ui2').enable({
-  enable = true, -- Whether to enable or disable the UI.
-  msg = { -- Options related to the message module.
-    ---@type 'cmd'|'msg' Default message target, either in the
-    ---cmdline or in a separate ephemeral message window.
-    ---@type string|table<string, 'cmd'|'msg'|'pager'> Default message target
-    ---or table mapping |ui-messages| kinds and triggers to a target.
+  enable = true,
+  msg = {
+    ---@type 'cmd'|'msg' Default message target, either in the cmdline or in a separate ephemeral message window.
+    ---@type string|table<string, 'cmd'|'msg'|'pager'> Default message target or table mapping |ui-messages| kinds and triggers to a target.
     targets = 'cmd',
-    cmd = { -- Options related to messages in the cmdline window.
-      height = 0.5 -- Maximum height while expanded for messages beyond 'cmdheight'.
-    },
-    dialog = { -- Options related to dialog window.
-      height = 0.5, -- Maximum height.
-    },
-    msg = { -- Options related to msg window.
-      height = 0.5, -- Maximum height.
-      timeout = 2000, -- Time a message is visible in the message window.
-    },
-    pager = { -- Options related to message window.
-      height = 5, -- Maximum height.
-    },
+    -- Maximum height while expanded for messages beyond 'cmdheight'.
+    cmd = {
+      height = 0.5
+      },
+    dialog = {
+      height = 0.5,
+      },
+    msg = { height = 0.5, timeout = 2000, },
+    pager = { height = 15, },
   },
 })
